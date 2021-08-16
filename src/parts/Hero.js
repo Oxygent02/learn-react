fc0f43b860a755a37dc829b8923f3d7bd60ef5f6
Oxygent02 <fy_danny@yahoo.com>
2021-08-16 22:17:14 +0700

    Refactor Hero Image

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import ImageHero from "assets/images/img-hero.png";
-import ImageFrame from "assets/images/img-hero-frame.png";
+import ImageHero from "assets/images/img-hero.jpg";
+import ImageFrame from "assets/images/img-hero-frame.jpg";
 import IconCities from "assets/images/icons/icon-cities.svg";
 import IconTraveler from "assets/images/icons/icon-traveler.svg";
 import IconTreasure from "assets/images/icons/icon-treasure.svg";
